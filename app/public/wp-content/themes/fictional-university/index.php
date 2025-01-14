085b2ae6d18f4d9287fe28c3ae68f072016020e4
Robyn <127926809+Robyn-web@users.noreply.github.com>
2025-01-14 07:31:32 +0000

    while loops

--- app/public/wp-content/themes/fictional-university/index.php
+++ app/public/wp-content/themes/fictional-university/index.php
@@ -1,11 +1,18 @@
 <?php
-function greet ($name, $colour){
-    echo "<p> Hi my name is $name and my favourite color is $colour.</p>";
+
+$names = array('Robyn', 'Rikke', 'Josephine');
+$count = 1;
+
+while ($count < 101) {
+echo "<li>$count</li>";
+$count++;
 }
-greet('John', 'blue');
-greet('Jane', 'orange');
+
 ?>
 
-<h1><?php bloginfo('name'); ?></h1>
+<p> Hi, my name is <?php echo $names[2]; ?>.</p>
 
-<p><?php bloginfo('description'); ?></p>
+<li>1</li>
+<li>2</li>
+<li>3</li>
+<li>4</li>

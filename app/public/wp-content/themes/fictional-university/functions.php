51505f02f3d51ecf17b11f7ff8ad9fc0f735e1a1
Robyn <127926809+Robyn-web@users.noreply.github.com>
2025-01-16 07:42:58 +0000

    imported files for build

--- app/public/wp-content/themes/fictional-university/functions.php
+++ app/public/wp-content/themes/fictional-university/functions.php
@@ -2,7 +2,8 @@
 
 // Loading style.css to site
 function university_files(){
-   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+   wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 // calls this university_files function

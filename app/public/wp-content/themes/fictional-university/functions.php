3093c15fe9c64b7b2e4bcd0011c1cd53c33808bf
Robyn <127926809+Robyn-web@users.noreply.github.com>
2025-01-16 07:54:06 +0000

    Added font awesome

--- app/public/wp-content/themes/fictional-university/functions.php
+++ app/public/wp-content/themes/fictional-university/functions.php
@@ -1,11 +1,11 @@
 <?php
 
 // Loading style.css to site
-function university_files(){
-   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+function university_files() {
+   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
-// calls this university_files function
+// Calls this university_files function
 add_action('wp_enqueue_scripts', 'university_files');
 
